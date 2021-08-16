f583ab656a7aaac94aaa24644808724e37877c43
Ulukbekk <ulukbekmomunkulov@gmail.com>
2021-08-16 17:11:39 +0600

    update db

--- Procfile
+++ Procfile
@@ -1,4 +1,4 @@
 web: pipenv rub python manage.py runserver 0.0.0.0:$PORT
 web: gunicorn core.wsgi:application --log-file - --log-level debug
 python manage.py collectstatic --noinput
-manage.py migrate
+
--- core/settings.py
+++ core/settings.py
@@ -94,9 +94,9 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'dasudlkfn9m9fj',
-        'USER': 'eshmyqixtfzinu',
-        'PASSWORD': '7f77204349258561e6bacacfddf111daff8469124ab10496d3e4e4f7a9d04cdf',
+        'NAME': 'd5rog84ocv3tot',
+        'USER': 'nymtleeaompjrn',
+        'PASSWORD': '158d694f20bcbc0a42a4722d970f63c3ce0925dc6ba72af24859cc4079762ee0',
         'HOST': 'ec2-3-217-68-126.compute-1.amazonaws.com',
         'PORT': '5432',
     }
